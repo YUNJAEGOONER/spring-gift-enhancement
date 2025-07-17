1c2f778ae10c719351ab0b0357409d4c28cad281
YUNJAEGOONER <coygyj@gmail.com>
2025-07-17 12:37:54 +0900

    feat: fetch join 적용

--- src/main/java/gift/repository/WishListRepository.java
+++ src/main/java/gift/repository/WishListRepository.java
@@ -1,6 +1,5 @@
 package gift.repository;
 
-import gift.dto.wish.WishResponseDto;
 import gift.entity.WishList;
 import java.util.List;
 import java.util.Optional;
@@ -11,7 +10,8 @@ import org.springframework.data.repository.query.Param;
 public interface WishListRepository extends JpaRepository<WishList, Long> {
     Optional<WishList> findWishListById(Long id);
     void removeWishListById(Long id);
-    List<WishList> findWishListByMemberId(Long memberId);
+    @Query("SELECT w FROM WishList w JOIN FETCH w.product WHERE w.member.id = :memberId")
+    List<WishList> findWishListByMemberId(@Param("memberId") Long memberId);
     Optional<WishList> findWishListByMemberIdAndProductId(Long memberId, Long productId);
 }
 
