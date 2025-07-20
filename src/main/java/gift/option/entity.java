f2f98ca5a7699abb1043ffe9e7facb26e47b7634
YUNJAEGOONER <coygyj@gmail.com>
2025-07-20 14:28:21 +0900

    feat: option 엔티티를 위한 ddl 작성 및 연관관계 설정

--- src/main/java/gift/option/entity.java
+++ src/main/java/gift/option/entity.java
@@ -1,10 +1,13 @@
 package gift.option;
 
+import gift.entity.Product;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class entity {
@@ -16,6 +19,10 @@ public class entity {
     private String name;
 
     private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id") //name_of_FK
+    private Product product;
 
     public void addStock(int amount){
         quantity += amount;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,26 +1,40 @@
-create table product(
-    id bigint auto_increment,
-    name varchar(255) not null,
-    price int not null,
+create table product
+(
+    id        bigint auto_increment,
+    name      varchar(255) not null,
+    price     int          not null,
     image_url varchar(255) not null,
     primary key (id)
 );
 
-create table member(
-    id bigint auto_increment,
-    email varchar(255) not null,
+create table member
+(
+    id       bigint auto_increment,
+    email    varchar(255) not null,
     password varchar(255) not null,
-    role varchar(10) not null,
+    role     varchar(10)  not null,
     primary key (id),
     unique (email)
 );
 
-create table wishlist(
-    id bigint auto_increment,
-    member_id bigint not null,
-    product_id bigint not null,
-    quantity int not null,
+create table option
+(
+    id         bigint auto_increment,
+    name       varchar(255) not null,
+    quantity   int          not null,
+    product_id bigint,
     primary key (id),
-    foreign key (product_id) references product(id),
-    foreign key (member_id) references member(id)
+    unique (name),
+    foreign key (product_id) references product(id)
+);
+
+create table wishlist
+(
+    id         bigint auto_increment,
+    member_id  bigint not null,
+    product_id bigint not null,
+    quantity   int    not null,
+    primary key (id),
+    foreign key (product_id) references product (id),
+    foreign key (member_id) references member (id)
 );
